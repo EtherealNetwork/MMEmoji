b091d9db5a7b27202bef530d7aaeeb1734728d63
Grasinga <Grasinga@gmail.com>
2014-07-30 21:26:19 -0600

    Edited

--- src/main/java/com/minemee/MMEmoji/MMEmoji.java
+++ src/main/java/com/minemee/MMEmoji/MMEmoji.java
@@ -8,10 +8,35 @@ public class MMEmoji extends JavaPlugin {
 
 	Logger log = Logger.getLogger("Minecraft");
 	
-	public final MMEmojiAscii MMEA = new MMEmojiAscii();
-	
 	public void onEnable(){
 		log.info("MMEmoji enabled!");
+		// Commands:
+		getCommand("tableflip").setExecutor(new MMEmojiAscii());
+		getCommand("table").setExecutor(new MMEmojiAscii());
+		getCommand("dtable").setExecutor(new MMEmojiAscii());
+		getCommand("finger").setExecutor(new MMEmojiAscii());
+		getCommand("shrug").setExecutor(new MMEmojiAscii());
+		getCommand("wtf").setExecutor(new MMEmojiAscii());
+		getCommand("why").setExecutor(new MMEmojiAscii());
+		getCommand("cool").setExecutor(new MMEmojiAscii());
+		getCommand("cry").setExecutor(new MMEmojiAscii());
+		getCommand("sad").setExecutor(new MMEmojiAscii());
+		getCommand("dead").setExecutor(new MMEmojiAscii());
+		getCommand("dance").setExecutor(new MMEmojiAscii());
+		getCommand("gimme").setExecutor(new MMEmojiAscii());
+		getCommand("bighit").setExecutor(new MMEmojiAscii());
+		getCommand("visualizer").setExecutor(new MMEmojiAscii());
+		getCommand("vis").setExecutor(new MMEmojiAscii());
+		getCommand("spam").setExecutor(new MMEmojiAscii());
+		getCommand("bunny").setExecutor(new MMEmojiAscii());
+		getCommand("fixtable").setExecutor(new MMEmojiAscii());
+		getCommand("throw").setExecutor(new MMEmojiAscii());
+		getCommand("erika").setExecutor(new MMEmojiAscii());
+		getCommand("donger").setExecutor(new MMEmojiAscii());
+		getCommand("wannago").setExecutor(new MMEmojiAscii());
+		getCommand("snip").setExecutor(new MMEmojiAscii());
+		getCommand("lilgimme").setExecutor(new MMEmojiAscii());
+		getCommand("kirk").setExecutor(new MMEmojiAscii());
 	}
  
 	public void onDisable(){
--- src/main/java/com/minemee/MMEmoji/MMEmojiAscii.java
+++ src/main/java/com/minemee/MMEmoji/MMEmojiAscii.java
@@ -1,129 +1,138 @@
 package com.minemee.MMEmoji;
 
-import org.bukkit.command.Command;
-import org.bukkit.command.CommandSender;
+import org.bukkit.command.*;
 import org.bukkit.entity.Player;
 
-public class MMEmojiAscii{
-	public boolean onCommand(CommandSender sender, Command cmd, String commandLabel, String[] args){
+public class MMEmojiAscii implements CommandExecutor{
+
+	public boolean onCommand(CommandSender sender, Command cmd, String commandLabel, String[] args) {
 		if (sender instanceof Player) 
 		{
-	           Player player = (Player) sender;
-	        
-		if(commandLabel.equalsIgnoreCase("spam")){
-			if(args.length == 0){
-				player.sendMessage("████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████");
+			Player player = (Player) sender;
+			if(commandLabel.equalsIgnoreCase("spam")){
+				if(args.length == 0){
+					player.sendMessage("████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████");
+				}
+				else if(args.length == 1){
+				  if(player.getServer().getPlayer(args[0]) != null){
+				     Player targetPlayer = player.getServer().getPlayer(args[0]);
+				     targetPlayer.sendMessage("████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████");
+				     return true;
+				  }
+				 else if(args.length == 2)
+				 {
+					 if(player.getServer().getPlayer(args[0]) != null){
+					     Player targetPlayer = player.getServer().getPlayer(args[0]);
+					     if(args[1].equalsIgnoreCase("tableflip"))
+					    	 for(int i=20;i>=0;i--)
+					    		 targetPlayer.sendMessage("(╯°□°）╯︵ ┻━┻");
+					     return true;
+					  }
+				 }
+				}
 			}
-			else if(args.length == 1){
-			  if(player.getServer().getPlayer(args[0]) != null){
-			     Player targetPlayer = player.getServer().getPlayer(args[0]);
-			     targetPlayer.sendMessage("████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████");
-			     return true;
-			  }
+			if(commandLabel.equalsIgnoreCase("tableflip") || commandLabel.equalsIgnoreCase("table")){
+			   player.chat("(╯°□°）╯︵ ┻━┻");
+			   return true;
 			}
-		}
-		if(commandLabel.equalsIgnoreCase("tableflip") || commandLabel.equalsIgnoreCase("table")){
-		   player.chat("(╯°□°）╯︵ ┻━┻");
-		   return true;
-		}
-		if(commandLabel.equalsIgnoreCase("dtable")){
-		   player.chat("┻━┻ ︵ヽ(`Д´)ﾉ︵﻿ ┻━┻");
-		   return true;
-		}
-		if(commandLabel.equalsIgnoreCase("finger")){
-		   player.chat("┌∩┐(◣_◢)┌∩┐");
-		   return true;
-		}
-		if(commandLabel.equalsIgnoreCase("shrug")){
-		   player.chat("¯\\_(ツ)_/¯");
-		   return true;
-		}
-		if(commandLabel.equalsIgnoreCase("wtf")){
-		   player.chat("(ಠ_ಠ)");
-		   return true;
-		}
-		if(commandLabel.equalsIgnoreCase("why")){
-		   player.chat("ლ(ಠ益ಠლ)");
-		   return true;
-		}
-		if(commandLabel.equalsIgnoreCase("cool")){
-		   player.chat("( •_•)");
-		   player.chat("( •_•)>⌐■-■");
-		   player.chat("(⌐■_■)");
-		   return true;
-		}
-		if(commandLabel.equalsIgnoreCase("cry")){
-		   player.chat("(╥﹏╥)");
-		   return true;
-		}
-		if(commandLabel.equalsIgnoreCase("sad")){
-		   player.chat("(╯︵╰,)");
-		   return true;
-		}
-		if(commandLabel.equalsIgnoreCase("dead")){
-		   player.chat("(✖╭╮✖)");
-		   return true;
-		}
-		if(commandLabel.equalsIgnoreCase("dance")){
-		   player.chat("♪┏(°.°)┛┗(°.°)┓┗(°.°)┛┏(°.°)┓ ♪");
-		   return true;
-		} 
-		if(commandLabel.equalsIgnoreCase("gimme")){
-		   player.chat("༼ つ ◕_◕ ༽つ");
-		   return true;
-		}
-		if(commandLabel.equalsIgnoreCase("bighit")){
-		   player.chat("█▬█ █ ▀█▀");
-		   return true;
-		}  
-		if(commandLabel.equalsIgnoreCase("visualizer") || commandLabel.equalsIgnoreCase("vis")){
-		   player.chat("▇ ▅ █ ▅ ▇ ▂ ▃ ▁ ▁ ▅ ▃ ▅ ▅ ▄ ▅ ▇");
-		   return true;
-		}
-		if(commandLabel.equalsIgnoreCase("bunny")){
-		   player.chat("(\\_/)");
-		   player.chat(" (^.^)");
-		   player.chat("O(\")(\")");
-		   return true;
-		}
-		if(commandLabel.equalsIgnoreCase("fixtable")){
-		   player.chat("┬──┬ﾉ(° -°ﾉ)");
-		   return true;
-		}
-		if(commandLabel.equalsIgnoreCase("throw")){
-		   player.chat("(ノಠ ∩ಠ)ノ彡( \\o°o)\\");
-		   return true;
-		}
-		if(commandLabel.equalsIgnoreCase("erika")){
-		   player.chat("༼☉ɷ⊙༽");
-		   return true;
-		}
-		if(commandLabel.equalsIgnoreCase("donger")){
-		   player.chat("ヽ༼ຈل͜ຈ༽ﾉ");
-		   return true;
-		}
-		if(commandLabel.equalsIgnoreCase("wannago")){
-		   player.chat("(ง •̀_•́)ง");
-		   return true;
-		}
-		if(commandLabel.equalsIgnoreCase("snip")){
-		   player.chat("( ＾◡＾)っ✂╰⋃╯");
-		   return true;
-		}
-		if(commandLabel.equalsIgnoreCase("lilgimme")){
-		   player.chat("(づ｡◕‿‿◕｡)づ");
-		   return true;
-		}
-		if(commandLabel.equalsIgnoreCase("kirk")){
-			player.chat("(◐ω◑ )");
-			return true;
-		}
+			if(commandLabel.equalsIgnoreCase("dtable")){
+			   player.chat("┻━┻ ︵ヽ(`Д´)ﾉ︵﻿ ┻━┻");
+			   return true;
+			}
+			if(commandLabel.equalsIgnoreCase("finger")){
+			   player.chat("┌∩┐(◣_◢)┌∩┐");
+			   return true;
+			}
+			if(commandLabel.equalsIgnoreCase("shrug")){
+			   player.chat("¯\\_(ツ)_/¯");
+			   return true;
+			}
+			if(commandLabel.equalsIgnoreCase("wtf")){
+			   player.chat("(ಠ_ಠ)");
+			   return true;
+			}
+			if(commandLabel.equalsIgnoreCase("why")){
+			   player.chat("ლ(ಠ益ಠლ)");
+			   return true;
+			}
+			if(commandLabel.equalsIgnoreCase("cool")){
+			   player.chat("( •_•)");
+			   player.chat("( •_•)>⌐■-■");
+			   player.chat("(⌐■_■)");
+			   return true;
+			}
+			if(commandLabel.equalsIgnoreCase("cry")){
+			   player.chat("(╥﹏╥)");
+			   return true;
+			}
+			if(commandLabel.equalsIgnoreCase("sad")){
+			   player.chat("(╯︵╰,)");
+			   return true;
+			}
+			if(commandLabel.equalsIgnoreCase("dead")){
+			   player.chat("(✖╭╮✖)");
+			   return true;
+			}
+			if(commandLabel.equalsIgnoreCase("dance")){
+			   player.chat("♪┏(°.°)┛┗(°.°)┓┗(°.°)┛┏(°.°)┓ ♪");
+			   return true;
+			} 
+			if(commandLabel.equalsIgnoreCase("gimme")){
+			   player.chat("༼ つ ◕_◕ ༽つ");
+			   return true;
+			}
+			if(commandLabel.equalsIgnoreCase("bighit")){
+			   player.chat("█▬█ █ ▀█▀");
+			   return true;
+			}  
+			if(commandLabel.equalsIgnoreCase("visualizer") || commandLabel.equalsIgnoreCase("vis")){
+			   player.chat("▇ ▅ █ ▅ ▇ ▂ ▃ ▁ ▁ ▅ ▃ ▅ ▅ ▄ ▅ ▇");
+			   return true;
+			}
+			if(commandLabel.equalsIgnoreCase("bunny")){
+			   player.chat("(\\_/)");
+			   player.chat(" (^.^)");
+			   player.chat("O(\")(\")");
+			   return true;
+			}
+			if(commandLabel.equalsIgnoreCase("fixtable")){
+			   player.chat("┬──┬ﾉ(° -°ﾉ)");
+			   return true;
+			}
+			if(commandLabel.equalsIgnoreCase("throw")){
+			   player.chat("(ノಠ ∩ಠ)ノ彡( \\o°o)\\");
+			   return true;
+			}
+			if(commandLabel.equalsIgnoreCase("erika")){
+			   player.chat("༼☉ɷ⊙༽");
+			   return true;
+			}
+			if(commandLabel.equalsIgnoreCase("donger")){
+			   player.chat("ヽ༼ຈل͜ຈ༽ﾉ");
+			   return true;
+			}
+			if(commandLabel.equalsIgnoreCase("wannago")){
+			   player.chat("(ง •̀_•́)ง");
+			   return true;
+			}
+			if(commandLabel.equalsIgnoreCase("snip")){
+			   player.chat("( ＾◡＾)っ✂╰⋃╯");
+			   return true;
+			}
+			if(commandLabel.equalsIgnoreCase("lilgimme")){
+			   player.chat("(づ｡◕‿‿◕｡)づ");
+			   return true;
+			}
+			if(commandLabel.equalsIgnoreCase("kirk")){
+				player.chat("(◐ω◑ )");
+				return true;
+			}
 		}
 		else
 		{
 			sender.sendMessage("Only players can use this command.");
 			return false;
-		}
+		}	           
 		return false;
-	}
+	}	
 }
